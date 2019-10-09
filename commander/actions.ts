0300ed85fe232d0a16c802894a79bb486194e8aa
Andrew A <aallbrig@gmail.com>
2019-10-09 10:00:00 -0400

    Command actions live in their own file now.

---
+++ commander/actions.ts
@@ -0,0 +1,24 @@
+import { testPrompt } from "../inquirer/prompts";
+import { debugLog, progLog } from "../utils/loggers";
+
+export const rollAction = async (listOfDice:string) => {
+  debugLog('roll command called!');
+  debugLog(`variable listOfDice value: ${listOfDice}`);
+  if (listOfDice) {
+    progLog('TODO: validate user input');
+    progLog('user input should be a list of dice e.g. "d4" or "d4,d6,d8,d10,d20,d100" or "d4,d4,d4"');
+  } else {
+    const answers = await testPrompt();
+    debugLog(`prompt response:\n${JSON.stringify(answers, null, '  ')}`);
+  }
+};
+export const walkThroughAction = async (walkThroughName:string) => {
+  debugLog('walk-through command called!');
+  debugLog(`variable walkThroughName value: ${walkThroughName}`);
+  if (walkThroughName) {
+    progLog('TODO: validate user input');
+  } else {
+    const answers = await testPrompt();
+    debugLog(`prompt response:\n${JSON.stringify(answers, null, '  ')}`);
+  }
+};
--- commander/dungeons-and-dragons-cli.ts
+++ commander/dungeons-and-dragons-cli.ts
@@ -1,45 +1,31 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-import { progLog, debugLog } from "../utils/loggers";
-import { testPrompt } from "../inquirer/prompts";
+import { rollAction, walkThroughAction } from "./actions";
+
+import { debugLog, progLog } from "../utils/loggers";
 
 const VERSION = '0.0.2';
 
-export const main = () => {
+export const dndCLI = (argv: string[]) => {
+  debugLog('D&D CLI called');
   const program = new Command();
   program.version(VERSION);
 
   program
   .command('roll [listOfDice]')
   .description('rolls dice')
-  .action(async (listOfDice) => {
-    debugLog('roll command called!');
-    debugLog(`variable listOfDice value: ${listOfDice}`);
-    if (listOfDice) {
-      progLog('TODO: validate user input');
-      progLog('user input should be a list of dice e.g. "d4" or "d4,d6,d8,d10,d20,d100" or "d4,d4,d4"');
-    } else {
-      const answers = await testPrompt();
-      debugLog(`prompt response:\n${JSON.stringify(answers, null, '  ')}`);
-    }
-  });
+  .action(rollAction);
 
   program
     .command('walk-through [walkThroughName]')
     .description('walk-through name of walk through (test)')
-    .action(async (walkThroughName) => {
-      debugLog('walk-through command called!');
-      debugLog(`variable walkThroughName value: ${walkThroughName}`);
-      if (walkThroughName) {
-        progLog('TODO: validate user input');
-      } else {
-        const answers = await testPrompt();
-        debugLog(`prompt response:\n${JSON.stringify(answers, null, '  ')}`);
-      }
-    });
+    .action(walkThroughAction);
 
-  program.parse(process.argv);
+  progLog(`Commands added: roll, walk-through`);
+  progLog(`Commands not added: server start/stop`);
+
+  program.parse(argv);
 };
 
-main();
+dndCLI(process.argv);
